88ecbba70d5d87733927a09319db533f03278423
sjoonl <junm4850@naver.com>
2021-09-08 23:47:04 +0900

    Create 01_chrome_test.py
    -chrome_test

---
+++ web_data/01_chrome_test.py
@@ -0,0 +1,36 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+url = "https://finance.naver.com/sise/"
+page = urlopen(url)
+
+soup = BeautifulSoup(page, 'html.parser')
+KOSPI = soup.find("span", id="KOSPI_now")
+
+KOSDAQ = soup.find("span", id="KOSDAQ_now")
+
+KOSPI_200 = soup.find("span", id="KPI200_now")
+
+popular = soup.find("ul", id="popularItemList")
+
+#select로 가져오는 방식
+top10_name = soup.select("#popularItemList > li > a")
+top10_price = soup.select("#popularItemList > li > span")
+
+#find all 로 가져오는 방식
+data = soup.find()
+
+
+for i in top10_name:
+    print(i.text)
+
+for i in top10_price:
+    print(i.text)
+
+
+print("코스피 현재 지수는: ", KOSPI.text)
+print("코스닥 현재 지수는: ", KOSDAQ.text)
+print("코스피200 지수는: ", KOSPI_200.text)
+
+print("인기 검색 종목", popular)
+
